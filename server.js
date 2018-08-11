27501bc7620758b765ac07235ad34fbde96ee576
lx kish <urban.ktulhu@gmail.com>
2018-08-12 09:54:51 +1200

    118 Node server and React app

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/users', (req, res) => {
+    res.json([
+        {
+            id:1,
+            username:'Alex'
+        },
+        {
+            id:2,
+            username:'Francis'
+        }
+    ])
+})
 
+app.get('/api/cars', (req, res) => {
+    res.json([
+        {
+            id:1,
+            brand:'Toyota'
+        },
+        {
+            id:2,
+            brand:'Volkswagen'
+        }
+    ])
+})
 
 const port = process.env.PORT || 3001;
 
